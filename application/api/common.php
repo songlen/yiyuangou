974d5dfd4109b170db9fb65817bf6623c20707df
shengli <363912809@qq.com>
2018-05-07 19:08:13 +0800

    ignore

--- application/api/common.php
+++ application/api/common.php
@@ -1,10 +1,7 @@
 <?php
 
 function response_success($data=[], $msg=''){
-// 允许 runapi.showdoc.cc 发起的跨域请求
-header("Access-Control-Allow-Origin: http://runapi.showdoc.cc"); 
-header("Access-Control-Allow-Credentials : true"); 
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie");
+
 	$result = array(
 		'code' => 200,
 		'data' => $data,
