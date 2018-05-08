4b342a4b523ec474144a55a48c51bc347dd42193
shengli <363912809@qq.com>
2018-05-08 16:02:07 +0800

    huodongye

--- application/api/common.php
+++ application/api/common.php
@@ -22,29 +22,4 @@ function response_error($data=[], $msg=''){
 
 	json($result, 400)->send();
 	exit;
-=======
-<?php
-
-function response_success($data=[], $msg=''){
-
-	$result = array(
-		'code' => 200,
-		'data' => $data,
-		'msg' => $msg,
-	);
-
-	json($result, 200)->send();
-	exit;
-}
-
-function response_error($data=[], $msg=''){
-	$result = array(
-		'code' => 400,
-		'data' => $data,
-		'msg' => $msg,
-	);
-
-	json($result, 400)->send();
-	exit;
->>>>>>> 6c99d03db863d2927ce1b4008f43a5862e456c83
 }
