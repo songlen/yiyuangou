61503190d027af1e7dcb0f6aa5f37a3fdaafad89
shengli <363912809@qq.com>
2018-06-04 08:06:44 +0800

    web

---
+++ web/js-server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by LiHongZhen on 2018/6/3.
+ */
+function setUserid(params) {
+    if (!params) return;
+    sessionStorage.setItem('userId', params);
+}
+
+function setGoodsInfo(params) {
+    if (!params) return;
+    sessionStorage.setItem('goodsInfo', params);
+}
