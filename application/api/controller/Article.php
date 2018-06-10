0b93130d6f110236e09192121e051076cfde49df
shengli <363912809@qq.com>
2018-06-10 21:08:13 +0800

    art

---
+++ application/api/controller/Article.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\api\controller;
+use think\Db;
+
+class Article extends Base {
+
+	public function __construct(){
+		// 设置所有方法的默认请求方式
+		$this->method = 'POST';
+
+		parent::__construct();
+	}
+    
+    // 夺宝详情
+    public function detail(){
+        $article_id = I('article_id');
+
+        // 活动详情
+        $info = M('Article')->field('title, content')->find($article_id);
+
+        if($info){
+            $info['content'] = html_entity_decode($info['content']);
+            response_success($info);
+        } else {
+            response_error('', '文章不存在');
+        }
+    }
+
+    
+}
