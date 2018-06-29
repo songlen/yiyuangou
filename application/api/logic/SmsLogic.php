7f13287f5d88caa3d7f786543b3978c8d91a5712
shengli <363912809@qq.com>
2018-06-29 15:30:51 +0800

    composer bug

--- application/api/logic/SmsLogic.php
+++ application/api/logic/SmsLogic.php
@@ -67,6 +67,9 @@ class SmsLogic {
 
         $smsLogid = Db::name('sms_log')->insertGetId($data);
 
+        // 执行短信网关发送
+        $this->exec();
+
         Db::name('sms_log')->where("id=$smsLogid")->update(array('status'=>'1'));
         
         return $code;
@@ -101,5 +104,29 @@ class SmsLogic {
         }
 
         return true;
+    }
+
+    private exec(){
+        require __DIR__ . '/vendor/autoload.php';
+        use Twilio\Rest\Client;
+
+        // Your Account SID and Auth Token from twilio.com/console
+        $account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+        $auth_token = 'your_auth_token';
+        // In production, these should be environment variables. E.g.:
+        // $auth_token = $_ENV["TWILIO_ACCOUNT_SID"]
+
+        // A Twilio number you own with SMS capabilities
+        $twilio_number = "+15017122661";
+
+        $client = new Client($account_sid, $auth_token);
+        $client->messages->create(
+            // Where to send a text message (your cell phone?)
+            '+15558675310',
+            array(
+                'from' => $twilio_number,
+                'body' => 'I sent this message in under 10 minutes!'
+            )
+        );
     }
 }
