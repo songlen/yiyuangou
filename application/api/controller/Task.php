dcd3398480178350ecb25292ae5d2f655cff708f
shengli <363912809@qq.com>
2018-08-17 23:25:37 +0800

    删除幸运号bug

--- application/api/controller/Task.php
+++ application/api/controller/Task.php
@@ -119,7 +119,7 @@ class Task extends Base {
 
                 // 更改订单状态为 已作废 5
                 Db::name('order')->where('order_id', $item['order_id'])->setField('order_status', 5);
-                Db::name('lucky_number')->where('order_id', $item['orders'])->delete();
+                Db::name('lucky_number')->where('order_id', $item['order_id'])->delete();
             }
         }
     }
