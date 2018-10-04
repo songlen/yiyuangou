cc1ffa24366a34587c7fca356b36c290025e45c6
shengli <363912809@qq.com>
2018-10-04 11:43:17 +0800

    t

--- application/api/controller/Task.php
+++ application/api/controller/Task.php
@@ -38,7 +38,7 @@ class Task extends Base {
                 $query->where($where);
             })
             ->whereOr(function($query){
-                $query->where(array('status'=>1, 'maiman_time'=>array('<>', NULL)));
+                $query->where(array('status'=>1, 'maiman_time'=>'not null'));
             })
             ->field('act_id, goods_id, goods_name')
             ->select();
